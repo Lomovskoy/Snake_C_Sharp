2e901c566cd580dc0a5d9af2f29b6d5839ae50c0
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-01-29 11:31:02 +0200

    Класс змеёка, применение обстракции

--- main/main/Program.cs
+++ main/main/Program.cs
@@ -24,6 +24,7 @@ namespace main
             Point p = new Point(4, 5, '*');// Создание обьекта класса
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
+            snake.Move();
         }
     }
 }
