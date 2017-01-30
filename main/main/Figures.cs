1a095292d041e8187baa292629ad423c1c6e404d
Lomovskoy <lomovskoy.kirill@yandex.ru>
2017-01-30 22:18:54 +0200

    Конец

--- main/main/Figures.cs
+++ main/main/Figures.cs
@@ -4,17 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace main
+namespace Snake
 {
-    class Figures // 
+    class Figure
     {
         protected List<Point> pList;
-        public void Drow()
+
+        public void Draw()
         {
             foreach (Point p in pList)
             {
                 p.Draw();
             }
+        }
+
+        internal bool IsHit(Figure figure)
+        {
+            foreach (var p in pList)
+            {
+                if (figure.IsHit(p))
+                    return true;
+            }
+            return false;
+        }
+
+        private bool IsHit(Point point)
+        {
+            foreach (var p in pList)
+            {
+                if (p.IsHit(point))
+                    return true;
+            }
+            return false;
         }
     }
 }
--- main/main/Snake.cs
+++ main/main/Snake.cs
@@ -4,27 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace main
+namespace Snake
 {
-    class Snake : Figures
+    class Snake : Figure
     {
         Direction direction;
-        public Snake(Point tail, int lenght, Direction _direction)
+
+        public Snake(Point tail, int length, Direction _direction)
         {
             direction = _direction;
             pList = new List<Point>();
-            for(int i = 0; i < lenght; i++)
+            for (int i = 0; i < length; i++)
             {
-                Point p = new Point( tail );
+                Point p = new Point(tail);
                 p.Move(i, direction);
                 pList.Add(p);
             }
         }
 
-        internal void Move()
+        public void Move()
         {
-            Point tail = pList.First();// First возвращает 1й элемент списка
-            pList.Remove(tail);// Remove удаление точки хвоста
+            Point tail = pList.First();
+            pList.Remove(tail);
             Point head = GetNextPoint();
             pList.Add(head);
 
@@ -32,12 +33,48 @@ namespace main
             head.Draw();
         }
 
-        public Point  GetNextPoint()
+        public Point GetNextPoint()
         {
-            Point head = pList.Last();// Текущая позиция головы змейки
+            Point head = pList.Last();
             Point nextPoint = new Point(head);
             nextPoint.Move(1, direction);
             return nextPoint;
+        }
+
+        public bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[i]))
+                    return true;
+            }
+            return false;
+        }
+
+        public void HandleKey(ConsoleKey key)
+        {
+            if (key == ConsoleKey.LeftArrow)
+                direction = Direction.LEFT;
+            else if (key == ConsoleKey.RightArrow)
+                direction = Direction.RIGHT;
+            else if (key == ConsoleKey.DownArrow)
+                direction = Direction.DOWN;
+            else if (key == ConsoleKey.UpArrow)
+                direction = Direction.UP;
+        }
+
+        public bool Eat(Point food)
+        {
+            Point head = GetNextPoint();
+            if (head.IsHit(food))
+            {
+                food.sym = head.sym;
+                pList.Add(food);
+                return true;
+            }
+            else
+                return false;
         }
     }
 }
